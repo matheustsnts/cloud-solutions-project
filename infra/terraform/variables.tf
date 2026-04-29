a2dd4ef032317924cb100ed1439983702b38e013
matheustsnts <matheusteixeras1@gmail.com>
2026-04-29 11:40:09 -0300

    feat(infra): implement resource group and vnet architecture

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "project_name" {
+  type    = string
+  default = "cloud-solutions"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
