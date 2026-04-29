f2d87f4fcf836265b98b0c9d266cb3b9a0cb1290
matheustsnts <matheusteixeras1@gmail.com>
2026-04-29 11:50:31 -0300

    feat(infra): add OCI provider configuration for multi-cloud strategy

---
+++ infra/oci/variables.tf
@@ -0,0 +1,9 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "region" { default = "us-ashburn-1" } # Ou a região que você escolheu no cadastro
+
+variable "compartment_id" {
+  description = "OCID do compartimento onde os recursos serão criados"
+}
